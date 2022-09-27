0ec78efb515088acb6901dc8b42b67e036ace05b
annalisegong <annalise.gong@cuw.edu>
2022-09-26 20:00:39 -0500

    added givenYears variables

--- src/net/codejava/Main.java
+++ src/net/codejava/Main.java
@@ -5,18 +5,19 @@ public class Main
 	public static void main(String[] args)
 	{
 		int dividedPopulation = 592307692;
+		int givenYears = 3;
 		int desiredYears = 50;
 		System.out.println("If 13 disciples can train 2 people per year, then it will take " 
-						+ yearsToConvert(dividedPopulation) + " years to convert everyone.");
+						+ yearsToConvert(dividedPopulation, givenYears) + " years to convert everyone.");
 		System.out.println();
 		System.out.println("If they want to convert everyone in " + desiredYears 
 						+ " years, then each discple must train " + numberToConvert
 						(dividedPopulation,desiredYears) + " people per year.");
 	}
 	
-	public static int yearsToConvert(int p)
+	public static int yearsToConvert(int p, int g)
 	{
-		int years = (p / 2) * 3;
+		int years = (p / 2) * g;
 		return years;
 	}
 	
